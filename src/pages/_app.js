7580fa40721bff202fb266b668632232b22dce16
Vipin <97426806+vpnbeni@users.noreply.github.com>
2024-07-31 11:14:51 +0530

    asdfsadf

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,10 +6,10 @@ export default function App({ Component, pageProps }) {
   const [isOpen,setIsOpen]=useState(false);
 
   return (
-    <div className="flex">
+    <div className="flex justify-between">
       <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
       {/* <div className="flex justify-end"> */}
-      <div className={`mt-20 ms-20  ${!isOpen?"w-[90%]":"w-[80%]"}`}>    
+      <div className={`mt-20 ms-20  absolute right-10  ${!isOpen?"w-[90%]":"w-[80%]"}`}>    
         <Component {...pageProps} />
         </div>
       {/* </div> */}
