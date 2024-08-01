1b508fa76f625dee487798cec11d731bda50088f
Vipin <97426806+vpnbeni@users.noreply.github.com>
2024-08-01 12:33:02 +0530

    sadfasf

--- src/component/cards/DashboardCard.jsx
+++ src/component/cards/DashboardCard.jsx
@@ -44,7 +44,11 @@ const DashboardCard = ({service, pageName, setData}) => {
 const UpdateModel = ({ service, refreshData }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [updateData, setUpdateData] = useState(service);
-  console.log(updateData,"updateData")
+  // console.log(updateData,"updateData")
+  const handleCancel=()=>{
+    setIsOpen(false);
+
+  }
     const handleUpdate = async () => {
       const res = await fetch(`${API_URL}auth/v1/service/${service._id}`, {
         method: "PUT",
@@ -87,12 +91,20 @@ const UpdateModel = ({ service, refreshData }) => {
                 onChange={(e) => setUpdateData(e.target.value)}
                 className="border-2 p-1 w-full mb-4"
               />
+              <div className="flex gap-2">
+              <button
+                onClick={handleCancel}
+                className="bg-gray-200 py-1 px-2 mt-4 rounded-md text-black"
+              >
+                Cancel
+              </button>
               <button
                 onClick={handleUpdate}
-                className="bg-slate-400 py-1 px-2 mt-4 rounded-md text-white"
+                className="bg-[#521950] py-1 px-2 mt-4 rounded-md text-white"
               >
                 Update
               </button>
+              </div>
             </div>
           </div>
         )}
@@ -104,7 +116,10 @@ const UpdateModel = ({ service, refreshData }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [metaTag, setMetaTag] = useState("");
     const [metaDescription, setMetaDescription] = useState("");
+    const handleCancel=()=>{
+      setIsOpen(false);
   
+    }
     const handleUpdate = async () => {
       const res = await fetch(`${API_URL}auth/v1/development/meta-tag/development-solution`, {
         method: "POST",
@@ -132,7 +147,9 @@ const UpdateModel = ({ service, refreshData }) => {
         </p>
         {isOpen && (
           <div className="fixed top-0 left-0 h-screen w-full bg-[rgba(0,0,0,0.66)] flex items-center justify-center">
-            <div className="bg-white p-4 rounded-md w-1/2 h-[240px] relative">
+            <div className="bg-white p-4 rounded-md w-1/2  relative">
+            <div className="flex justify-start my-4 font-bold text-lg">Update Slug </div>
+
               <button
                 onClick={() => setIsOpen(false)}
                 className="absolute top-2 right-2 p-1 text-black"
@@ -151,12 +168,21 @@ const UpdateModel = ({ service, refreshData }) => {
                 placeholder="Meta Description"
                 className="border-2 p-1 w-full mb-4"
               />
+              <div className="flex gap-2">
+              <button
+                onClick={handleCancel}
+                className="bg-gray-200 py-1 px-2 mt-4 rounded-md text-black"
+              >
+                Cancel
+              </button>
               <button
                 onClick={handleUpdate}
-                className="bg-slate-400 py-1 px-2 mt-4 rounded-md text-white"
+                className="bg-[#521950] py-1 px-2 mt-4 rounded-md text-white"
               >
                 Update
               </button>
+              </div>
+             
             </div>
           </div>
         )}
